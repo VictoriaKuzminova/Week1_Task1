b9f31dc263ea35f03c3a830b5062d626ac7d7514
VictoriaKuzminova <119403787+VictoriaKuzminova@users.noreply.github.com>
2022-11-30 17:45:22 +0300

    Add files via upload

---
+++ Графическое приложение 1_2/2/2/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace _2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Num_textBox = new System.Windows.Forms.TextBox();
+            this.res_button = new System.Windows.Forms.Button();
+            this.res_textBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(27, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(269, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите двузначное число:";
+            // 
+            // Num_textBox
+            // 
+            this.Num_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Num_textBox.Location = new System.Drawing.Point(327, 54);
+            this.Num_textBox.Name = "Num_textBox";
+            this.Num_textBox.Size = new System.Drawing.Size(232, 30);
+            this.Num_textBox.TabIndex = 1;
+            this.Num_textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Num_textBox_KeyPress);
+            // 
+            // res_button
+            // 
+            this.res_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.res_button.Location = new System.Drawing.Point(234, 109);
+            this.res_button.Name = "res_button";
+            this.res_button.Size = new System.Drawing.Size(163, 40);
+            this.res_button.TabIndex = 2;
+            this.res_button.Text = "Результат";
+            this.res_button.UseVisualStyleBackColor = true;
+            this.res_button.Click += new System.EventHandler(this.res_button_Click);
+            // 
+            // res_textBox
+            // 
+            this.res_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.res_textBox.Location = new System.Drawing.Point(32, 179);
+            this.res_textBox.Name = "res_textBox";
+            this.res_textBox.Size = new System.Drawing.Size(527, 229);
+            this.res_textBox.TabIndex = 3;
+            this.res_textBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 450);
+            this.Controls.Add(this.res_textBox);
+            this.Controls.Add(this.res_button);
+            this.Controls.Add(this.Num_textBox);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Сравнение ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Num_textBox;
+        private System.Windows.Forms.Button res_button;
+        private System.Windows.Forms.RichTextBox res_textBox;
+    }
+}
+
---
+++ Графическое приложение 1_2/2/2/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2
+{
+    public partial class Form1 : Form
+    {
+        public int num;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void res_button_Click(object sender, EventArgs e)
+        {
+            if (Num_textBox.Text == "")
+            {
+                MessageBox.Show("Введены не все данные");
+            }
+            else
+            {
+                try
+                {
+                    num = int.Parse(Num_textBox.Text);
+                    if (num > 0)
+                    {
+                        if (num <= 10 || num >= 100)
+                        {
+                            MessageBox.Show("Число должно быть двузначным!");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        if (num <= -100 || num >= -10)
+                        {
+                            MessageBox.Show("Число должно быть двузначным!");
+                            return;
+                        }
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Некорректный ввод! Доступен только ввод чисел, запятой и знака \"минус\"");
+                }
+                int num1;
+                if (num < 0)
+                {
+                    num1 = -1 * (num / 10);
+                }
+                else
+                {
+                    num1 = num / 10;
+                }
+                int num2;
+                if (num > 0)
+                {
+                    num2 = num % 10;
+                }
+                else
+                {
+                    num2 = -1 * (num % 10);
+                }
+                if (num1 > num2) res_textBox.Text = "Первое число " + num1 + " " + "\nВторое число " + num2 + "\nПервое число больше второго";
+                else if (num1 < num2) res_textBox.Text = "Первое число " + num1 + " " + "\nВторое число " + num2 + "\nПервое число меньше второго";
+                else res_textBox.Text = "Первое число " + num1 + " " + "\nВторое число " + num2 + "\nЧисла равны";
+
+            }
+        }
+
+        private void Num_textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string allowSymbol = "-\b";
+            if (!allowSymbol.Contains(e.KeyChar) && !char.IsNumber(e.KeyChar))
+                e.Handled = true;
+        }
+    }
+}
+
